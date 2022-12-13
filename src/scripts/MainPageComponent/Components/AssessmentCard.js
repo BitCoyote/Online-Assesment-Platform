0483f5be5e0ca3ee339e055acb63c8cea2bac733
BitCoyote <fanjoey1210@gmail.com>
2022-12-13 17:25:14 -0500

    Fix: 2 buttons

--- src/scripts/MainPageComponent/Components/AssessmentCard.js
+++ src/scripts/MainPageComponent/Components/AssessmentCard.js
@@ -3,11 +3,13 @@ import {ButtonKMQ} from "../../Components/ButtonKMQ";
 import { submitRetakeAssessment } from "../../../api/assessments";
 
 const AssessmentCard = ({assessment, img, savedItem, user_id}) => {
-    const handleRetakeTest = () => {
-        console.log(user_id, assessment.test_id)
-        submitRetakeAssessment({user_id, test_id: assessment.test_id}).then(res => {
-           window.location.href='/assessment/id-' + assessment.test_id;
-        })
+    const isSaved = (savedItem?.answers_obj || "[]") !== "[]";
+    const isFinished = (savedItem?.quiz_finished || "") === "1";
+    const handleGetResult = () => {
+        if(isFinished)
+            window.location.href="/get-results/id-" + assessment.test_id;
+        else
+            alert("You have to finish the test.");
     }
 
     return <div className={'inline-block w-1/4 m-8 p-3 border border-slate-200 rounded h-96 relative'}>
@@ -20,26 +22,12 @@ const AssessmentCard = ({assessment, img, savedItem, user_id}) => {
             <h2 className={'text-sm mb-2'}>{assessment.test_title}</h2>
         </div>
         <div className={'absolute bottom-0 left-0 w-full text-center px-1'}>
-        {
-            savedItem ? savedItem.quiz_finished === "1" ? (
-                <React.Fragment>
-                    <a href={'/get-results/id-' + assessment.test_id}>
-                        <ButtonKMQ text={'Show Results'} dark className={'w-full my-2' }/>
-                    </a>
-                    <a onClick={handleRetakeTest}>
-                        <ButtonKMQ text={'Take Test'} className={'w-full my-2'}/>
-                    </a>
-                </React.Fragment>
-            ) : (
-                <a href={'/assessment/id-' + assessment.test_id}>
-                    <ButtonKMQ text={'Resume Test'} className={'w-full my-2'}/>
-                </a>
-            ) : (
-                <a onClick={handleRetakeTest}>
-                    <ButtonKMQ text={'Take Test'} className={'w-full my-2'}/>
-                </a>
-            )
-        }
+            <a href={"/assessment/id-" + assessment.test_id}>
+                <ButtonKMQ text={(isSaved && !isFinished) ? 'Resume Test' : 'Take Test'} className={'w-full my-2'}/>
+            </a>
+            <a onClick={() => handleGetResult()} >
+                <ButtonKMQ text={'Show Results'} dark className={'w-full my-2' }/>
+            </a>
         </div>
     </div>
 }
