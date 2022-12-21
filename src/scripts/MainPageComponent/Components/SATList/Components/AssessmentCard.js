9fa4f892fd5fc4c08d590257280bf38128796afd
hryhorchuk117 <hryhorchuk117@gmail.com>
2022-12-20 20:36:04 -0500

    main-page: new design implemented

---
+++ src/scripts/MainPageComponent/Components/SATList/Components/AssessmentCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AssessmentCard = ({assessment, savedItem}) => {
+    const isSaved = (savedItem?.answers_obj || "[]") !== "[]";
+    const isFinished = (savedItem?.quiz_finished || "") === "1";
+
+    return <div className={'cursor-pointer align-top inline-block w-1/4 mr-16 mb-8 p-6 border-2 border-solid border-slate-400 h-52 relative'}
+                onClick={() => {
+                    document.location.href = (isFinished ? "/get-results/id-" : "/assessment/id-") + assessment.test_id
+                }}>
+        <div className={'h-8'}>
+            {
+                isFinished
+                    ? <span className={'float-right bg-[#d1efdf] text-[#22a265] px-4 py-0 rounded-2xl text-[.9rem]'}>
+                        Completed
+                      </span>
+                    : isSaved ? <span className={'float-right bg-[#fceadd] text-[#ff9252] px-4 py-0 rounded-2xl text-[.9rem]'}>
+                                    In Progress
+                                </span>
+                              : null
+            }
+        </div>
+        <div className={'text-2xl mb-4'}>
+            {assessment.test_title.split('(')[assessment.test_title.split('(').length - 1].slice(0, -1)}
+        </div>
+        <div className={'text-md h-24'}>
+            {assessment.test_title.split('(').slice(0, -1).join(' ')}
+        </div>
+    </div>
+}
+
+export default AssessmentCard;
--- src/scripts/MainPageComponent/index.js
+++ src/scripts/MainPageComponent/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
-import { useGetAllAssessments, getAssessmentStatus } from "../../api/assessments";
-import AssessmentCard from "./Components/AssessmentCard";
+import React, {useEffect, useState} from "react"
+import {useGetAllAssessments, getAssessmentStatus} from "../../api/assessments";
+import AssessmentCard from "./Components/SATList/Components/AssessmentCard";
 import FrontPage1 from '../../assets/assessments/assessment_frontpage_1.png';
 import FrontPage2 from '../../assets/assessments/assessment_frontpage_2.png';
 import FrontPage3 from '../../assets/assessments/assessment_frontpage_3.png';
@@ -8,44 +8,53 @@ import FrontPage4 from '../../assets/assessments/assessment_frontpage_4.png';
 import FrontPage5 from '../../assets/assessments/assessment_frontpage_5.png';
 import FrontPage6 from '../../assets/assessments/assessment_frontpage_6.png';
 import FrontPage7 from '../../assets/assessments/assessment_frontpage_7.png';
-import { useAccount } from "../../api/utils";
+import {useAccount} from "../../api/utils";
 import Loading from "../Helpers/Loading";
 import Error from "../Helpers/Error";
 import {ButtonKMQ} from "../Components/ButtonKMQ";
+import TabsMenu from "./Components/Tabs/TabsMenu";
+import SATList from "./Components/SATList";
+
+const MainPageTabs = ['SAT', 'Results'];
 
 const MainPageComponent = () => {
-    const cardImages = [FrontPage1, FrontPage2, FrontPage3, FrontPage4, FrontPage5, FrontPage6, FrontPage7];
     const [user, accountLoading, authError] = useAccount('me');
     const [data, loading, error] = useGetAllAssessments({user_id: user?.id});
     const [assessmentStatus, setAssessmentStatus] = useState([]);
+    const [currTab, setCurrTab] = useState(MainPageTabs[0]);
+
 
     useEffect(() => {
-        if(user) {
+        if (user) {
             getAssessmentStatus({user_id: user?.id})
-            .then((status) => {
-                try {
-                    setAssessmentStatus(JSON.parse(status));
-                } catch (e) {
-                    return;
-                }
-            })
+                .then((status) => {
+                    try {
+                        setAssessmentStatus(JSON.parse(status));
+                    } catch (e) {
+                        return;
+                    }
+                })
         }
     }, [user])
 
     return (
-        <div className={'py-24'}>
-            {(loading || accountLoading) && (<Loading />)}
-            {(error || authError) && (<Error msg={error.message} />)}
-            {data && data.SAT_Assessments && data.SAT_Assessments.map((item, index) => 
-                <AssessmentCard 
-                    assessment={item}
-                    user_id={user?.id} 
-                    img={cardImages[index % cardImages.length]} 
-                    savedItem={assessmentStatus.find(e => {
-                        return (parseInt(e.quiz_id) === parseInt(item.test_id));
-                    }
-                    )}
-                />)}
+        <div className={'pb-24 table'}>
+            {(loading || accountLoading) && (<Loading/>)}
+            {(error || authError) && (<Error msg={error.message}/>)}
+            <div className={'w-1/5 table-cell border-r-2 border-solid border-slate-200 align-top'}>
+                <TabsMenu
+                    currTab={currTab}
+                    setCurrTab={setCurrTab}
+                    allTabs={MainPageTabs}
+                />
+            </div>
+            <div className={'w-4/5 table-cell'}>
+                <SATList
+                    data={data}
+                    user={user}
+                    assessmentStatus={assessmentStatus}
+                />
+            </div>
         </div>
     )
 }
