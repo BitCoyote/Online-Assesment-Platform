7913eb2d2c710096469d7ff81751fd727658c62e
BitCoyote <fanjoey1210@gmail.com>
2023-01-06 17:05:09 -0500

    Implemented: React pages are focused in one page, React.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php get_header();?>
 <div class="">
-  <!-- example react component -->
-  <div id="render-react-here"></div>
-  <!-- end example react component -->
-
   <div class="prose max-w-full">
   </div>
 </div>
