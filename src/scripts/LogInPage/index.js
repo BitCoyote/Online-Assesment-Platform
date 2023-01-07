0d54f186b61c5dd059433661937892051090a561
BitCoyote <fanjoey1210@gmail.com>
2023-01-06 19:17:22 -0500

    Fix: remove horizontal bar(replace w-screen as w-full)

--- src/scripts/LogInPage/index.js
+++ src/scripts/LogInPage/index.js
@@ -32,7 +32,7 @@ const LogInPage = () => {
         }
     }, [window.location.href, loading])
 
-    return <div className={'w-screen table'}>
+    return <div className={'w-full table'}>
         <div className={'table-cell w-[55vw] relative'}>
             <div className={'w-1/2 text-center absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]'}>
                 <div className={'text-4xl font-bold mb-12'}>Welcome</div>
